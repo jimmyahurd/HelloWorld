ee00557685396eeae6b7308098301b3d53b2f8a6
jimmy <tuh01663@temple.edu>
2019-09-16 00:30:37 -0400

    Added a button that allows user to change textView to read "You have clicked the button.".

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,13 +3,28 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button changeText;
+    TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        changeText = findViewById(R.id.changeText);
+        message = findViewById(R.id.message);
+
+        changeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                message.setText("You have clicked the button.");
+            }
+        });
     }
 }
